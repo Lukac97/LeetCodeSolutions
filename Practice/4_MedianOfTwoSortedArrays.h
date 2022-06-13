dc553213a90e4e77a1d78ba5f853b35304abf25f
Lukac97 <alecar97@gmail.com>
2022-06-13 19:34:26 +0200

    Update 4, more modular code.

--- Practice/4_MedianOfTwoSortedArrays.h
+++ Practice/4_MedianOfTwoSortedArrays.h
@@ -6,10 +6,55 @@ pair<int, int> findRoughMedian(const vector<int>& nums, int start, int end)
 	return make_pair(nums[mid], mid);
 }
 
-double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+double calculateMedianOfEvenArray(vector<int>& nums1, vector<int>& nums2, const int& leftChunkSize, const int& end1)
 {
-	int leftChunkSize = (int) ceil((nums1.size() + nums2.size()) / 2.0);
+	int leftNum;
+	int rightNum;
+	if (end1 <= 0)
+	{
+		leftNum = nums2[leftChunkSize - end1 - 1];
+	}
+	else if (leftChunkSize - end1 <= 0)
+	{
+		leftNum = nums1[end1 - 1];
+	}
+	else
+	{
+		leftNum = max(nums1[end1 - 1], nums2[leftChunkSize - end1 - 1]);
+	}
 
+	if (end1 >= nums1.size())
+	{
+		rightNum = nums2[leftChunkSize - end1];
+	}
+	else if (leftChunkSize - end1 >= nums2.size())
+	{
+		rightNum = nums1[end1];
+	}
+	else
+	{
+		rightNum = min(nums1[end1], nums2[leftChunkSize - end1]);
+	}
+
+	return (leftNum + rightNum) / 2.0;
+}
+
+double calculateMedianOfUnevenArray(vector<int>& nums1, vector<int>& nums2, const int& leftChunkSize, const int& end1)
+{
+	if (end1 <= 0)
+	{
+		return nums2[leftChunkSize - end1 - 1];
+	}
+	if (leftChunkSize - end1 <= 0)
+	{
+		return nums1[end1 - 1];
+	}
+
+	return max(nums1[end1 - 1], nums2[leftChunkSize - end1 - 1]);
+}
+
+int findNumberOfLeftItemsOfFirstArray(vector<int>& nums1, vector<int>& nums2, const int& leftChunkSize)
+{
 	int end1 = min(leftChunkSize, nums1.size());
 	int prevEnd1 = end1;
 
@@ -37,50 +82,23 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
 		}
 	}
 
+	return end1;
+
+}
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+{
+	int leftChunkSize = (int) ceil((nums1.size() + nums2.size()) / 2.0);
+
+	int end1 = findNumberOfLeftItemsOfFirstArray(nums1, nums2, leftChunkSize);
+
 	if ((nums1.size() + nums2.size()) % 2 == 0)
 	{
-		int leftNum;
-		int rightNum;
-		if (end1 <= 0)
-		{
-			leftNum = nums2[leftChunkSize - end1 - 1];
-		}
-		else if (leftChunkSize - end1 <= 0)
-		{
-			leftNum = nums1[end1 - 1];
-		}
-		else
-		{
-			leftNum = max(nums1[end1 - 1], nums2[leftChunkSize - end1 - 1]);
-		}
-
-		if (end1 >= nums1.size())
-		{
-			rightNum = nums2[leftChunkSize - end1];
-		}
-		else if (leftChunkSize - end1 >= nums2.size())
-		{
-			rightNum = nums1[end1];
-		}
-		else
-		{
-			rightNum = min(nums1[end1], nums2[leftChunkSize - end1]);
-		}
-
-		return (leftNum + rightNum) / 2.0;
+		return calculateMedianOfEvenArray(nums1, nums2, leftChunkSize, end1);
 	}
 	else
 	{
-		if (end1 <= 0)
-		{
-			return nums2[leftChunkSize - end1 - 1];
-		}
-		if (leftChunkSize - end1 <= 0)
-		{
-			return nums1[end1 - 1];
-		}
-
-		return max(nums1[end1 - 1], nums2[leftChunkSize - end1 - 1]);
+		return calculateMedianOfUnevenArray(nums1, nums2, leftChunkSize, end1);
 	}
 
 }
